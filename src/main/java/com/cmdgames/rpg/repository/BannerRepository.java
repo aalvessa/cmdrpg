370032b6f4646356c5b8ccd486d72e1e255ab786
anderson_sa <anderson_sa@outlook.com>
2018-11-09 22:39:17 -0200

    resource reading fix

--- src/main/java/com/cmdgames/rpg/repository/BannerRepository.java
+++ src/main/java/com/cmdgames/rpg/repository/BannerRepository.java
@@ -5,18 +5,16 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BannerRepository {
-
-
+    
     public String getFileContent(String filePath) throws URISyntaxException, IOException {
-        Path path = Paths.get(getClass().getClassLoader().getResource(filePath).toURI());
-        Stream<String> lines = Files.lines(path);
-        String data = lines.collect(Collectors.joining("\n"));
-        lines.close();
-        return data;
+        Scanner s = new Scanner(getClass().getClassLoader().getResourceAsStream(filePath)).useDelimiter("\\A");
+        String result = s.hasNext() ? s.next() : "";
+        return result;
     }
 
 }
